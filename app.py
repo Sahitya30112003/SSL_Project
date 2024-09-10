4143f68fb967257118ee87ba418b57367ecb5a8a
Sahitya30112003 <101263991+Sahitya30112003@users.noreply.github.com>
2024-09-10 17:27:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
-import numpy as np
+
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sklearn
 
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
